aad4559ce7e52f9cc386f2f4d8022030481e9d2f
Stephen Trevorrow <strevorrow@problemsolutions.net>
2012-06-25 10:42:30 -0400

    added initial version of code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(name="pytincan", 
+      version="0.0.1", 
+      author="Steve Trevorrow", 
+      author_email="streverrow@problemsolutions.net", 
+      packages=find_packages(),
+      long_description="Python package to interact with the TinCanAPI"
+      install_requires=["requests>=0.13.1"])
