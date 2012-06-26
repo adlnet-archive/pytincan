0c5e265fbdb2655812c7690b94e4fcceb14bc542
Stephen Trevorrow <strevorrow@problemsolutions.net>
2012-06-26 17:08:56 -0400

    Added filtering

--- tincan/__init__.py
+++ tincan/__init__.py
@@ -2,6 +2,7 @@ from requests.auth import HTTPBasicAuth
 import requests
 import json
 import urllib
+import uuid
 class tincanStatement(object):
 	def __init__(self,userName,secret,endpoint,objectID,logger=None):
 		self._userName = userName
@@ -17,11 +18,25 @@ class tincanStatement(object):
 				            data=json.dumps(jsonObject),
 				            auth=HTTPBasicAuth(self._userName,self._secret),
 				            headers={"Content-Type":"application/json"})
-			a = self.getFilteredStatements(_verb="interacted",_registration="1111-1111-1111-1111",_context=True,_actor={'name':"Steve"},_limit=30,_authoritative=False,_sparse=True,_instructor={'name':"Steve"})
-			return resp
+			self.getFilteredStatements(_verb="created", _context=True)
 		except IOError as e:
 			if self.logger is not None:
 				self.logger.error(e)
+	
+	def submitStatementList(self, jsonObjectList):
+		for statement in jsonObjectList:
+			
+			try:
+				print statement
+				resp = requests.post(self._endpoint,
+				            data=json.dumps(statement),
+				            auth=HTTPBasicAuth(self._userName,self._secret),
+				            headers={"Content-Type":"application/json"})
+				print resp
+			except IOError as e:
+				if self.logger is not None:
+					self.logger.error(e)
+	
 	def getLastStatement(self):
 		try:
 			url = self._endpoint+"?statementId="+self._objectID
@@ -52,42 +67,58 @@ class tincanStatement(object):
 			if self.logger is not None:
 				self.logger.error(e)
 	def getFilteredStatements(self,_verb=None,_object=None,_registration=None,_context=None,_actor=None,_since=None,_until=None,_limit=None,_authoritative=None,_sparse=None,_instructor=None):
-		url = self._endpoint
-		
+		url = self._endpoint +"?"
+		statementJSON ={}
 		if(_verb != None):
-			url +="?verb="+str(verb)+"&"
-		
+			url +="verb="+_verb+"&"
+			statementJSON['verb'] = _verb
 		if(_object != None):
-			url +="?object="+str(_object)+"&"
-		
+			url +="object="+str(_object)+"&"
+			statementJSON['object'] = _object
 		if(_registration != None):
-			url +="?registration="+str(_registration)+"&"
-		
+			url +="registration="+str(_registration)+"&"
+			statementJSON['registration'] = _registration
 		if(_context != None):
-			url +="?context="+str(_context)+"&"
-		
+			url +="context="+str(_context)+"&"
+			statementJSON['context'] = _context
 		if(_actor != None):
-			url +="?actor="+str(_actor)+"&"
-		
+			url +="actor="+str(_actor)+"&"
+			statementJSON['actor'] = _actor
 		if(_since != None):
-			url +="?since="+str(_since)+"&"
-		
+			url +="since="+str(_since)+"&"
+			statementJSON['since'] = _since
 		if(_until != None):
-			url +="?until="+str(_until)+"&"
-		
+			url +="until="+str(_until)+"&"
+			statementJSON['until'] = _until
 		if(_limit != None):
-			url +="?limit="+str(_limit)+"&"
-		
+			url +="limit="+str(_limit)+"&"
+			statementJSON['limit'] = _limit
 		if(_authoritative != None):
-			url +="?authoritative="+str(_authoritative)+"&"
-		
+			url +="authoritative="+str(_authoritative)+"&"
+			statementJSON['authoritative'] = _authoritative
 		if(_sparse != None):
-			url +="?sparse="+str(_sparse)+"&"
-		
+			url +="sparse="+str(_sparse)+"&"
+			statementJSON['sparse'] = _sparse
 		if(_instructor != None):
-			url +="?instructor="+str(_instructor)+"&"
-
-		if(url.endswith("&")):
-			url[:-1]
-
-		return url
+			url +="instructor="+str(_instructor)
+			statementJSON['instructor'] = _instructor
+		if(url.endswith('&')):
+			url = str(url[:-1])
+			
+		if (True):
+			
+			print type(statementJSON)
+			print statementJSON
+			resp = requests.post(self._endpoint,
+						        data=statementJSON,
+						        auth=HTTPBasicAuth(self._userName,self._secret))
+			
+			print resp.text
+			return resp
+		else:
+			resp = requests.get(url,
+								auth=HTTPBasicAuth(self._userName,self._secret),
+								headers={"Content-Type":"application/json"})
+			
+			return resp
+			
