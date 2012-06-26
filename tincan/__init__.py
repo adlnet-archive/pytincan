3bb46aa574e320536c55890a7a61c1b24d6786b5
Stephen Trevorrow <strevorrow@problemsolutions.net>
2012-06-26 10:57:50 -0400

    Added statement retrieval: all, by ID, previous statement inserted

--- tincan/__init__.py
+++ tincan/__init__.py
@@ -1,24 +1,52 @@
 from requests.auth import HTTPBasicAuth
 import requests
 import json
+import urllib
 class tincanStatement(object):
-	def __init__(self,userName,secret,endpoint,logger=None):
+	def __init__(self,userName,secret,endpoint,objectID,logger=None):
 		self._userName = userName
 		self._secret = secret
 		self._endpoint = endpoint
+		self._objectID =objectID
 		self.logger = logger
-	
+		
 
 	def submitStatement(self, jsonObject):	  
-
 		try:
 			resp = requests.post(self._endpoint,
 				            data=json.dumps(jsonObject),
 				            auth=HTTPBasicAuth(self._userName,self._secret),
 				            headers={"Content-Type":"application/json"})
 			return resp
-
 		except IOError as e:
 			if self.logger is not None:
-				self.logger.error(e)	
-
+				self.logger.error(e)
+	def getLastStatement(self):
+		try:
+			url = self._endpoint+"?statementId="+self._objectID
+			resp = requests.get(url,
+							auth=HTTPBasicAuth(self._userName,self._secret),
+							headers={"Content-Type":"application/json"})
+			return resp
+		except IOError as e:
+			if self.logger is not None:
+				self.logger.error(e)
+	def getStatementbyID(self, ID):
+		try:
+			url = self._endpoint+"?statementId="+ID
+			resp = requests.get(url,
+							auth=HTTPBasicAuth(self._userName,self._secret),
+							headers={"Content-Type":"application/json"})
+			return resp
+		except IOError as e:
+			if self.logger is not None:
+				self.logger.error(e)
+	def getAllStatements(self):
+		try:
+			resp = requests.get(self._endpoint,
+							auth=HTTPBasicAuth(self._userName,self._secret),
+							headers={"Content-Type":"application/json"})
+			return resp
+		except IOError as e:
+			if self.logger is not None:
+				self.logger.error(e)
