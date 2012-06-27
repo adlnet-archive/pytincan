db657976fcb187465111c0ce8193692e1d465c5c
Stephen Trevorrow <strevorrow@problemsolutions.net>
2012-06-27 15:18:05 -0400

    Added Unit tests

--- src/tincan/__init__.py
+++ src/tincan/__init__.py
@@ -4,21 +4,20 @@ import json
 import urllib
 import uuid
 class tincanStatement(object):
-	def __init__(self,userName,secret,endpoint,objectID,logger=None):
+	def __init__(self,userName,secret,endpoint,logger=None):
 		self._userName = userName
 		self._secret = secret
 		self._endpoint = endpoint
-		self._objectID =objectID
 		self.logger = logger
 		
 
 	def submitStatement(self, jsonObject):	  
 		try:
+			print jsonObject
 			resp = requests.post(self._endpoint,
 				            data=json.dumps(jsonObject),
 				            auth=HTTPBasicAuth(self._userName,self._secret),
 				            headers={"Content-Type":"application/json"})
-			self.getFilteredStatements(_verb="created", _context=True)
 		except IOError as e:
 			if self.logger is not None:
 				self.logger.error(e)
@@ -27,33 +26,22 @@ class tincanStatement(object):
 		for statement in jsonObjectList:
 			
 			try:
-				print statement
 				resp = requests.post(self._endpoint,
 				            data=json.dumps(statement),
 				            auth=HTTPBasicAuth(self._userName,self._secret),
 				            headers={"Content-Type":"application/json"})
-				print resp
+
 			except IOError as e:
 				if self.logger is not None:
 					self.logger.error(e)
 	
-	def getLastStatement(self):
-		try:
-			url = self._endpoint+"?statementId="+self._objectID
-			resp = requests.get(url,
-							auth=HTTPBasicAuth(self._userName,self._secret),
-							headers={"Content-Type":"application/json"})
-			return resp
-		except IOError as e:
-			if self.logger is not None:
-				self.logger.error(e)
 	def getStatementbyID(self, ID):
 		try:
 			url = self._endpoint+"?statementId="+ID
+
 			resp = requests.get(url,
-							auth=HTTPBasicAuth(self._userName,self._secret),
-							headers={"Content-Type":"application/json"})
-			return resp
+								auth=HTTPBasicAuth(self._userName,self._secret))
+			return resp.json
 		except IOError as e:
 			if self.logger is not None:
 				self.logger.error(e)
@@ -62,63 +50,52 @@ class tincanStatement(object):
 			resp = requests.get(self._endpoint,
 							auth=HTTPBasicAuth(self._userName,self._secret),
 							headers={"Content-Type":"application/json"})
-			return resp
+			return resp.json
 		except IOError as e:
 			if self.logger is not None:
 				self.logger.error(e)
 	def getFilteredStatements(self,_verb=None,_object=None,_registration=None,_context=None,_actor=None,_since=None,_until=None,_limit=None,_authoritative=None,_sparse=None,_instructor=None):
-		url = self._endpoint +"?"
 		statementJSON ={}
 		if(_verb != None):
-			url +="verb="+_verb+"&"
 			statementJSON['verb'] = _verb
 		if(_object != None):
-			url +="object="+str(_object)+"&"
-			statementJSON['object'] = _object
+			statementJSON['object'] = json.dumps(_object)
 		if(_registration != None):
-			url +="registration="+str(_registration)+"&"
 			statementJSON['registration'] = _registration
 		if(_context != None):
-			url +="context="+str(_context)+"&"
+
 			statementJSON['context'] = _context
 		if(_actor != None):
-			url +="actor="+str(_actor)+"&"
-			statementJSON['actor'] = _actor
+
+			statementJSON['actor'] = json.dumps(_actor)
+
 		if(_since != None):
-			url +="since="+str(_since)+"&"
+
 			statementJSON['since'] = _since
 		if(_until != None):
-			url +="until="+str(_until)+"&"
+
 			statementJSON['until'] = _until
 		if(_limit != None):
-			url +="limit="+str(_limit)+"&"
+
 			statementJSON['limit'] = _limit
 		if(_authoritative != None):
-			url +="authoritative="+str(_authoritative)+"&"
+
 			statementJSON['authoritative'] = _authoritative
 		if(_sparse != None):
-			url +="sparse="+str(_sparse)+"&"
+
 			statementJSON['sparse'] = _sparse
 		if(_instructor != None):
-			url +="instructor="+str(_instructor)
-			statementJSON['instructor'] = _instructor
-		if(url.endswith('&')):
-			url = str(url[:-1])
-			
-		if (True):
-			
-			print type(statementJSON)
-			print statementJSON
+
+			statementJSON['instructor'] = json.dumps(_instructor)
+		
+		url = self._endpoint +"?"+ urllib.urlencode(statementJSON)
+		if (len(url)> 2048):	
 			resp = requests.post(self._endpoint,
-						        data=statementJSON,
-						        auth=HTTPBasicAuth(self._userName,self._secret))
-			
-			print resp.text
-			return resp
+							    data=statementJSON,
+							    auth=HTTPBasicAuth(self._userName,self._secret))
+			return resp.json
 		else:
 			resp = requests.get(url,
-								auth=HTTPBasicAuth(self._userName,self._secret),
-								headers={"Content-Type":"application/json"})
-			
-			return resp
-			
+						    auth=HTTPBasicAuth(self._userName,self._secret))
+			return resp.json
+		
