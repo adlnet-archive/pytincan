12e50bfde40c39b2dd2c70fa46d3030476afb23f
Stephen Trevorrow <strevorrow@problemsolutions.net>
2012-06-29 12:23:42 -0400

    Added simple regex validation on emails

--- src/tincan/dataValidation.py
+++ src/tincan/dataValidation.py
@@ -1,3 +1,5 @@
+import re
+
 def validateVerb(verb):
 	##Builds a list of valid verbs
 	valid_verbs=['experienced','attended','attempted','completed','passed','failed','answered','interacted','imported','created','shared','voided']	
@@ -6,4 +8,9 @@ def validateVerb(verb):
 
 def validateAgent(agent):
 	##Checks to see if the agent object contains the unique identifier email(mbox)
-	return 'mbox' in agent
+	email_regex = re.compile(r"^mailto:[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+	if 'mbox' in agent:
+		for email in agent['mbox']:
+			if(not bool(email_regex.match(email))):
+				return False
+		return True
